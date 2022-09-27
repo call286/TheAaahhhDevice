95ed4a8172064adc1cc1dd5ec8e237beb3710962
call286 <borgman@gmx.net>
2022-09-27 21:04:37 +0200

    Added DFPlayer Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,156 +1,92 @@
 #include <Arduino.h>
 
+#include <SoftwareSerial.h>
+#include <DFRobotDFPlayerMini.h>
+
+#define DF_RX D7
+#define DF_TX D6
+
+SoftwareSerial mySoftwareSerial(DF_RX, DF_TX); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+void printDetail(uint8_t type, int value);
+
 #include <FastLED.h>
-#define NUM_LEDS 1
+#define NUM_LEDS 2
 #define LED_DATA_PIN D8
 
 CRGBArray<NUM_LEDS> leds;
-/***************************************************
-  This is an example for the Adafruit VS1053 Codec Breakout
-
-  Designed specifically to work with the Adafruit VS1053 Codec Breakout
-  ----> https://www.adafruit.com/products/1381
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
-// include SPI, MP3 and SD libraries
-#include <SPI.h>
-#include <Adafruit_VS1053.h>
-#include <SD.h>
-
-// These are the pins used for the breakout example
-#define BREAKOUT_RESET -1 // VS1053 reset pin (output)
-#define BREAKOUT_CS D1    // VS1053 chip select pin (output)
-#define BREAKOUT_DCS D0   // VS1053 Data/command select pin (output)
-
-// These are common pins between breakout and shield
-#define CARDCS D4 // Card chip select pin
-// DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
-#define DREQ D3 // VS1053 Data request, ideally an Interrupt pin
-
-Adafruit_VS1053_FilePlayer musicPlayer =
-    // create breakout-example object!
-    Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
-
-////
-
-void printDirectory(File dir, int numTabs);
+CRGB aaahColor = CRGB(255, 213, 0);
+uint_fast8_t fractionPart = 0;
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("Adafruit VS1053 Library Test");
+  mySoftwareSerial.begin(9600);
 
-  // initialise the music player
-  if (!musicPlayer.begin())
-  { // initialise the music player
-    Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
-    while (1)
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+  if (!myDFPlayer.begin(mySoftwareSerial))
+  { // Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while (true)
       ;
   }
-  Serial.println(F("VS1053 found"));
+  Serial.println(F("DFPlayer Mini online."));
 
-  // musicPlayer.sineTest(0x44, 500); // Make a tone to indicate VS1053 is working
+  myDFPlayer.setTimeOut(500); // Set serial communictaion time out 500ms
 
-  if (!SD.begin(CARDCS))
-  {
-    Serial.println(F("SD failed, or not present"));
-    while (1)
-      ; // don't do anything more
-  }
-  Serial.println("SD OK!");
+  //----Set volume----
+  myDFPlayer.volume(20);   // Set volume value (0~30).
+  myDFPlayer.volumeUp();   // Volume Up
+  myDFPlayer.volumeDown(); // Volume Down
 
-  // list files
-  printDirectory(SD.open("/"), 0);
+  //----Set different EQ----
+  myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
+  //  myDFPlayer.EQ(DFPLAYER_EQ_POP);
+  //  myDFPlayer.EQ(DFPLAYER_EQ_ROCK);
+  //  myDFPlayer.EQ(DFPLAYER_EQ_JAZZ);
+  //  myDFPlayer.EQ(DFPLAYER_EQ_CLASSIC);
+  //  myDFPlayer.EQ(DFPLAYER_EQ_BASS);
 
-  // Set volume for left, right channels. lower numbers == louder volume!
-  musicPlayer.setVolume(255, 40);
+  //----Set device we use SD as default----
+  //  myDFPlayer.outputDevice(DFPLAYER_DEVICE_U_DISK);
+  myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
+  //  myDFPlayer.outputDevice(DFPLAYER_DEVICE_AUX);
+  //  myDFPlayer.outputDevice(DFPLAYER_DEVICE_SLEEP);
+  //  myDFPlayer.outputDevice(DFPLAYER_DEVICE_FLASH);
 
-  /***** Two interrupt options! *******/
-  // This option uses timer0, this means timer1 & t2 are not required
-  // (so you can use 'em for Servos, etc) BUT millis() can lose time
-  // since we're hitchhiking on top of the millis() tracker
-  // musicPlayer.useInterrupt(VS1053_FILEPLAYER_TIMER0_INT);
+  //----Mp3 control----
+  //  myDFPlayer.sleep();     //sleep
+  //  myDFPlayer.reset();     //Reset the module
+  //  myDFPlayer.enableDAC();  //Enable On-chip DAC
+  //  myDFPlayer.disableDAC();  //Disable On-chip DAC
+  //  myDFPlayer.outputSetting(true, 15); //output setting, enable the output and set the gain to 15
+  myDFPlayer.next();  //Play next mp3
+  //----Read imformation----
+  Serial.println(myDFPlayer.readState()); // read mp3 state
 
-  // This option uses a pin interrupt. No timers required! But DREQ
-  // must be on an interrupt pin. For Uno/Duemilanove/Diecimilla
-  // that's Digital #2 or #3
-  // See http://arduino.cc/en/Reference/attachInterrupt for other pins
-  // *** This method is preferred
-  if (!musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT))
-    Serial.println(F("DREQ pin is not an interrupt pin"));
-
+  Serial.println("FastLED init");
   FastLED.addLeds<WS2812, LED_DATA_PIN, GRB>(leds, NUM_LEDS); // GRB ordering is typical
-  FastLED.setBrightness(0);
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+  FastLED.show(); // Update the display
 }
 
 void loop()
 {
-  // Alternately, we can just play an entire file at once
-  // This doesn't happen in the background, instead, the entire
-  // file is played and the program will continue when it's done!
-  musicPlayer.playFullFile("ahhh001.mp3");
-  // musicPlayer.playFullFile("fart001.mp3");
-
-  // Start playing a file, then we can do stuff while waiting for it to finish
-  if (!musicPlayer.startPlayingFile("/choir001.mp3"))
+  EVERY_N_SECONDS(2)
   {
-    Serial.println("Could not open file choir001.mp3");
-    while (1)
-      ;
+    //----Read imformation----
+    Serial.println(myDFPlayer.readState()); // read mp3 state
   }
-  Serial.println(F("Started playing"));
-
-  while (musicPlayer.playingMusic)
+  EVERY_N_MILLISECONDS(25)
   {
-    // file is now playing in the 'background' so now's a good time
-    // to do something else like handling LEDs or buttons :)
-    Serial.print(".");
-    FastLED.delay(33);
-    // Turn the LED on, then pause
-    leds[0] = CRGB::Orchid;
-    FastLED.show();
-    fadeToBlackBy(leds,1,20);
-  }
-  Serial.println("");
-  Serial.println("Done playing music");
-}
-
-/// File listing helper
-void printDirectory(File dir, int numTabs)
-{
-  while (true)
-  {
-
-    File entry = dir.openNextFile();
-    if (!entry)
+    if (fractionPart <= 255)
     {
-      // no more files
-      // Serial.println("**nomorefiles**");
-      break;
+      CRGB newColor = blend(CRGB::Black, aaahColor, fractionPart);
+      fill_solid(leds, NUM_LEDS, newColor);
+      FastLED.show(); // Update the pixel display
+      fractionPart += 5;
     }
-    for (uint8_t i = 0; i < numTabs; i++)
-    {
-      Serial.print('\t');
-    }
-    Serial.print(entry.name());
-    if (entry.isDirectory())
-    {
-      Serial.println("/");
-      printDirectory(entry, numTabs + 1);
-    }
-    else
-    {
-      // files have sizes, directories do not
-      Serial.print("\t\t");
-      Serial.println(entry.size(), DEC);
-    }
-    entry.close();
   }
 }
